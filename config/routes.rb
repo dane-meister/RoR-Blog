73c05a8a3aa06931e41c0650f97170b03a9ec038
dane-meister <dane.meister@stonybrook.edu>
2024-06-13 00:33:19 -0400

    Implement CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -9,18 +9,10 @@ Rails.application.routes.draw do
   # root "posts#index"
 end
 
-Rails.application.routes.draw do
-  get "/articles", to: "articles#index"
-
-  # For details on the DSL available within this file,
-  # see https://guides.rubyonrails.org/routing.html
-
-end
 
 Rails.application.routes.draw do
   root "articles#index"
-
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
+  
+  resources :articles
 end
 
