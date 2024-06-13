1d5289ce215cbd6d158dd9e4642b9e8a8f6f9c0e
dane-meister <dane.meister@stonybrook.edu>
2024-06-13 03:31:11 -0400

    security improvements...

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-    http_basic_authenticate_with name: "dhh", password: "secret",
+    http_basic_authenticate_with name: "admin", password: "secret",
     only: :destroy
 
     def create
